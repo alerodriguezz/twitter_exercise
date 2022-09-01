d00ca93f349b6f15232ec6b01a4b9fd5fd85d268
arod <alexrodriguez218@yahoo.com>
2022-09-01 17:26:58 +0000

    added data analyzation that displays as a pie chart .png

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def fetch_data():
     data = pd.read_csv("elonmusk_tweets.csv")
@@ -8,7 +10,38 @@ def fetch_data():
 
 def analyze_data(my_dict):
 
-    print(type(my_dict))
+    #print(type(my_dict['text']))
+    analyzer = SentimentIntensityAnalyzer()
+
+    temp=[]
+
+    for i in my_dict['text']:
+        text = str(i)
+        ps = analyzer.polarity_scores(text)
+        temp.append({'text':text, 'compound':ps['compound']})
+    
+    tweetdf = pd.DataFrame(temp)
+    
+    count=dict(positive=0,neutral=0,negative=0)
+
+    for i in tweetdf['compound']:
+        if i >= 0.05:
+                count['positive']+=1
+        elif i <= -0.05:
+                 count['negative']+=1
+        else:
+                 count['neutral']+=1
+
+
+   
+
+    # Plot
+    plt.pie(count.values(), labels=count.keys())
+
+    plt.axis('equal')
+    plt.savefig('sample.png')
+
 
 if __name__=="__main__":
     analyze_data(fetch_data())
+
