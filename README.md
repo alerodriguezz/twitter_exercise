1f66e0287048e9bdf718fd8ae51399965f125cad
arod <alexrodriguez218@yahoo.com>
2022-09-02 12:54:17 +0000

    added line graph

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 exercise 02 of certik technical assesment. Elon Musk Twitter Sentiment Analysis.
 
 This program takes input of a csv file of elon musks tweets. 
+
+Output is some png's 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from datetime import datetime
 
 def fetch_data():
-    data = pd.read_csv("elonmusk_tweets.csv")
+    data = pd.read_csv("elonmusk_tweets.csv",parse_dates=True)
     data_dict = {col: list(data[col]) for col in data.columns}
 
     return data_dict
@@ -11,17 +13,20 @@ def fetch_data():
 def analyze_data(my_dict):
 
     #print(type(my_dict['text']))
+
     analyzer = SentimentIntensityAnalyzer()
+    
+    sentiments=[]
 
-    temp=[]
-
+    #sentiment analysis
     for i in my_dict['text']:
         text = str(i)
         ps = analyzer.polarity_scores(text)
-        temp.append({'text':text, 'compound':ps['compound']})
-    
-    tweetdf = pd.DataFrame(temp)
-    
+        sentiments.append({'text':text, 'compound':ps['compound']})
+    #sentiment df
+    tweetdf = pd.DataFrame(sentiments)
+    #print(tweetdf)
+    #Pie chart code
     count=dict(positive=0,neutral=0,negative=0)
 
     for i in tweetdf['compound']:
@@ -33,13 +38,33 @@ def analyze_data(my_dict):
                  count['neutral']+=1
 
 
-   
-
-    # Plot
     plt.pie(count.values(), labels=count.keys())
 
     plt.axis('equal')
-    plt.savefig('sample.png')
+    plt.savefig('pie_chart.png')
+    plt.close('all')
+
+    #line graph code 
+    #print(my_dict['created_at'])
+
+    a = [datetime.strptime(d, '%Y-%m-%d %H:%M:%S') for d in my_dict['created_at']]
+
+    x = matplotlib.dates.date2num(a)
+
+    formatter = matplotlib.dates.DateFormatter('%Y')
+
+    figure = plt.figure()
+
+    axes = figure.add_subplot(1, 1, 1)
+    axes.xaxis.set_major_formatter(formatter)
+
+    axes.plot(x, [sentiments[i]['compound'] for i in range(len(sentiments))])
+    # set axis titles
+    plt.xlabel("Time")
+    plt.ylabel("Scores")
+    # set chart title
+    plt.title("Elon Musk tweet sentiment over time")
+    plt.savefig('line_graph.png')
 
 
 if __name__=="__main__":
